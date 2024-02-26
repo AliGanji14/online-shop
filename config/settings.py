89465a4f3b4e1cead0928c7cfb7ee1cf7d510c06
Ali Ganji <aliganji142879@gmail.com>
2024-02-26 23:23:23 +0330

    add translation to produt app

--- config/settings.py
+++ config/settings.py
@@ -125,12 +125,12 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
