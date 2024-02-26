871ef4f082743e8f8d0d757fa2af704f0649621e
Ali Ganji <aliganji142879@gmail.com>
2024-02-26 10:24:18 +0330

    create manager active comments and make templatetags

--- products/models.py
+++ products/models.py
@@ -19,6 +19,11 @@ class Product(models.Model):
         return reverse('product_detail', kwargs={'pk': self.pk})
 
 
+class ActiveCommentsManager(models.Manager):
+    def get_queryset(self):
+        return super(ActiveCommentsManager, self).get_queryset().filter(active=True)
+
+
 class Comment(models.Model):
     PRODUCT_STARS = [
         ('1', 'Very Bad'),
@@ -34,6 +39,10 @@ class Comment(models.Model):
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+
+    # Manager
+    objects = models.Manager()
+    active_comments_manager = ActiveCommentsManager()
 
     def get_absolute_url(self):
         return reverse('product_detail', kwargs={'pk': self.product.id})
