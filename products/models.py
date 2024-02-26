8decafdbea0a0f8a5b7086c88550bf11c95e3100
Ali Ganji <aliganji142879@gmail.com>
2024-02-26 13:08:04 +0330

    add TabularInline in admin

--- products/models.py
+++ products/models.py
@@ -33,9 +33,14 @@ class Comment(models.Model):
         ('5', 'Perfect'),
     ]
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments', )
-    body = models.TextField()
-    stars = models.CharField(max_length=10, choices=PRODUCT_STARS)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Comment author'
+    )
+    body = models.TextField(verbose_name='Comment text')
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name='What is your stars?', )
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
