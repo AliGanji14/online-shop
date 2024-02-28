5ceabb0b3a4b9c1889c3d68e4a690f59884733d2
Ali Ganji <aliganji142879@gmail.com>
2024-02-28 16:26:45 +0330

    create cart and method add and save

---
+++ cart/cart.py
@@ -0,0 +1,30 @@
+class Cart:
+    def __init__(self, request, ):
+        """
+        Initialize the cart
+        """
+        self.request = request
+        self.session = request.session
+
+        cart = self.session.get('cart')
+        if not cart:
+            cart = self.session['cart'] = {}
+        self.cart = cart
+
+    def add(self, product, quantity=1, ):
+        """
+        Add a product to the cart
+        """
+        product_id = str(product.id)
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': quantity}
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.sava()
+
+    def sava(self, ):
+        """
+        Save the cart
+        """
+        self.session.modified = True
