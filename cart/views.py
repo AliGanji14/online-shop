612256dd694ab92ee6847d442015df5e0b552bf4
Ali Ganji <aliganji142879@gmail.com>
2024-03-02 18:05:35 +0330

    make view add in cart

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from products.models import Product
+from .cart import Cart
+from .forms import AddToCartProductForm
 
 
 def cart_detail_view(request):
-    return render(request, 'cart/cart.html', )
+    cart = Cart(request)
+    return render(request, 'cart/cart_detail.html', {
+        'cart': cart,
+    })
+
+
+def add_cart_view(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = AddToCartProductForm(request.POST)
+
+    if form.is_valid():
+        clean_deta = form.cleaned_data
+        quantity = clean_deta['quantity']
+        cart.add(product, quantity)
+    return redirect('cart:cart_detail')
