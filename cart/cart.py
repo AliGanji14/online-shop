74e3553d45a72a5e621ead03ce8227fdcf7b2b4e
Ali Ganji <aliganji142879@gmail.com>
2024-03-04 18:43:28 +0330

    show total amount

--- cart/cart.py
+++ cart/cart.py
@@ -50,6 +50,7 @@ class Cart:
             cart[str(product.id)]['product_obj'] = product
 
         for item in cart.values():
+            item['total_price'] = item['product_obj'].price * item['quantity']
             yield item
 
     def __len__(self):
@@ -60,6 +61,4 @@ class Cart:
         self.save()
 
     def get_total_price(self):
-        product_id = self.cart.keys()
-        products = Product.objects.filter(id__in=product_id)
-        return sum(product.price for product in products)
+        return sum(item['quantity'] * item['product_obj'].price for item in self.cart.values())
